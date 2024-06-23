a374c3a4cd8dea0c7470489b7211d474988b3bf3
Matz <mateushoneey@outlook.com.br>
2024-06-23 20:43:16 -0300

    Inicio do formulário de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { Checkbox } from "@nextui-org/checkbox";
+import { Switch } from "@nextui-org/switch";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      hello world
+      <h1 className="text-lg">Por favor, conecte-se para continuar.</h1>
+      <form action="" className="flex flex-col w-1/3 gap-y-4">
+        <Input
+          type="email"
+          variant="faded"
+          label="Email"
+          placeholder="Digite seu email"
+        />
+        <Input
+          type="password"
+          variant="faded"
+          label="Senha"
+          placeholder="Digite sua senha"
+        />
+        <Switch defaultSelected aria-label="Automatic updates" />
+        <Checkbox size="sm" defaultSelected>
+          Confirme os termos para prosseguir
+        </Checkbox>
+        <Button type="submit">Entrar</Button>
+      </form>
     </section>
   );
 }
